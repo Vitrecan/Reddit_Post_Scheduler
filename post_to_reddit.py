90343a6bef00dcb5949c14396679ddf461a97b32
Vitrecan <vitrecan@gmail.com>
2025-02-06 21:37:56 +1100

    Text Change

--- post_to_reddit.py
+++ post_to_reddit.py
@@ -10,10 +10,22 @@ def main():
 **Job Type**: Full-time, 24/7
 **Responsibilities**:
 * Willingness to participate in Clan Wars even if your Archer Queen is down.
-* Regularly contribute to clan
+* Regularly contribute to clan donations, because sharing is caring, and we care a lot.
 * Must get 3 stars in all attacks no matter what.
 * Must talk in chat no matter what time it is.
-"""
+* Must do Clan Games even if you accidentally kicked your phone off the bridge.
+**Qualifications**:
+* Minimum of 0 trophies (we're elite, but we're not snobs).
+* Experience in always winning in every attack.
+* A sense of humor that can withstand the worst Dad jokes from our dead chat.
+* Must not be afraid of several Clan War losses.
+**Perks**:
+* Be paid in troop donations (such as Goblins).
+* Clan War League Bonuses offered to our top 5 employees of the event.
+* An opportunity to be promoted to Elder.
+**How to Apply**:
+Send us your resume on the "I like to join your clan" message (our clan is invite only) or just drop down the word "Reddit" if you're looking for an unpaid position in our clan! Make sure to be fully maxed.
+~~The above was a joke plz join our clan we dont care if ur a rushed th1 if thats even a thing :)~~"""
     
     # Make the post
     success = create_reddit_post(
